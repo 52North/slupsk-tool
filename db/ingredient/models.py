c9512571fa39871357c7f2769e1565f98c4d593a
Martin Pontius <m.pontius@52north.org>
2019-08-28 13:29:18 +0200

    Use the name field in the list views

--- db/ingredient/models.py
+++ db/ingredient/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
+import pystache
 
 class Ingredient(models.Model):
     name = models.TextField(
         verbose_name="Name of ingredient",
     )
+
+    wq_label_template = "{{name}}"
+
+    def __str__(self):
+        return pystache.render(self.wq_label_template, self)
 
     class Meta:
         verbose_name = "ingredient"

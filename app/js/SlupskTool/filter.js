1de71990ec5467a50603b9b8b2a848e203438296
Martin Pontius <m.pontius@52north.org>
2020-01-14 16:48:04 +0100

    Extend select menu filter with date filtering

--- app/js/SlupskTool/filter.js
+++ app/js/SlupskTool/filter.js
@@ -6,74 +6,78 @@ var filter = {
 
 var $;
 
-var kindergartenIDs = new Map( [[ "Przedszkole Miejskie nr 1 Zaczarowany Ogród", 1 ],
-[ "Przedszkole Miejskie nr 2", 2 ],
-[ "Przedszkole Miejskie nr 3", 3 ],
-[ "Przedszkole Miejskie nr 4 Królestwo Skrzatów", 4 ],
-[ "Przedszkole Miejskie nr 5", 5 ],
-[ "Przedszkole Miejskie nr 6", 6 ],
-[ "Przedszkole Miejskie nr 7", 7 ],
-[ "Przedszkole Miejskie Integracyjne nr 8", 8 ],
-[ "Przedszkole Miejskie nr 9 Akademia Uśmiechu", 9 ],
-[ "Przedszkole Miejskie nr 10 Świat Fantazji", 10 ],
-[ "Przedszkole Miejskie nr 11 Calineczka", 11 ],
-[ "Przedszkole Miejskie nr 12 Niezapominajka ", 12 ],
-[ "Przedszkole Miejskie nr 15", 13 ],
-[ "Przedszkole Miejskie nr 19 Słupskiego Chłopczyka", 14 ],
-[ "Przedszkole Miejskie nr 23 Promyczek", 15 ],
-[ "Przedszkole Miejskie nr 24 Słupski Niedźwiadek Szczęścia", 16 ],
-[ "Przedszkole Miejskie nr 31 Bajkowa Kraina", 17 ],
-[ "Przedszkole Miejskie Integracyjne nr 32 Tęczowa Kraina", 18 ]] );
-
-
 filter.init = function (config) {
     $ = config && config.jQuery || window.jQuery;
 };
 
 filter.run = function ($page, routeInfo) {
 
-  $page.on('change', 'select[name=kindergarten_id]', function(evt) {
-      var $button = $(evt.target);
-      var $parent = $button.parent();
-      var kindergarten = $parent.find("span")[0].innerText;  // TODO: trim white space
-      var kindergartenID = kindergartenIDs.get(kindergarten);
+  // Does this event trigger correctly? (console.log() is not executed each time the selection changes...)
+  $page.on('change', 'select[id=dishrating-kindergarten_id]', function(evt) {
+      var kindergartenID = $(evt.target).val().trim();
+      var date = $('#dishrating-date').val().trim();
 
-      ds.get('/kindergartendishs').then(function(dishes) {
+      _filter(kindergartenID, date);
 
-        var dishesFiltered = _filter(dishes, kindergartenID);
+  });
 
-        var $dish_select = $('[id="dishrating-dish_id"]');
-        $dish_select.find('option').remove().end();
-        $dish_select.append('<option value="">Select one...</option>');
+  $page.on('change', 'input[id=dishrating-date]', function(evt) {
+      var date = $(evt.target).val().trim();
+      var kindergartenID = $('#dishrating-kindergarten_id').val().trim();
 
-        var value = 1;
-
-        for (let dish of dishesFiltered) {
-           $dish_select.append('<option value="' + value + '">' + dish + '</option>');
-           value++;
-        }
-
-      });
+      _filter(kindergartenID, date);
 
   });
 
 
 };
 
-function _filter(dishes, kindergartenID) {
+function _filter(kindergartenID, date) {
 
-  var dishesFiltered = [];
+  // 1) Use wq's store.js app to get the full list of dishes
+  // 2) Filter the list of dishes
+  // 3) Remove all options of the select menu and append only those from the filtered list
 
-  for (let dish of dishes.list) {
-    if (dish.kindergarten_id === kindergartenID) {
-      dishesFiltered.push(dish.label);
+  ds.get('/kindergartendishs').then(function(dishes) {
+
+    var dishesFiltered = [];
+    var dishIds = [];
+
+    for (let dish of dishes.list) {
+      if (dish.kindergarten_id.toString() === kindergartenID && date === "") {
+        dishesFiltered.push(dish.label);
+        dishIds.push(dish.id);
+        continue;
+      }
+      if (kindergartenID === "" && dish.date === date) {
+        dishesFiltered.push(dish.label);
+        dishIds.push(dish.id);
+        continue;
+      }
+      if (dish.kindergarten_id.toString() === kindergartenID && dish.date === date) {
+        dishesFiltered.push(dish.label);
+        dishIds.push(dish.id);
+        continue;
+      }
+      if (kindergartenID === "" && date === "") {
+        dishesFiltered.push(dish.label);
+        dishIds.push(dish.id);
+        continue;
+      }
     }
-  }
 
-  return dishesFiltered;
+    var $dish_select = $('[id="dishrating-dish_id"]');
+    $dish_select.find('option').remove().end();
+    $dish_select.append('<option value="">Select one...</option>');
+
+    // Note that value is the private key of the dish
+    for (var i = 0; i < dishesFiltered.length; i++ ) {
+      $dish_select.append('<option value="' + dishIds[i] + '">' + dishesFiltered[i] + '</option>');
+    }
+
+  });
 
 }
-
 
 return filter;
 

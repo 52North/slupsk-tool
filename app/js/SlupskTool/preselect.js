40cf8109c418fb7ddd6195ea008d97f4c440197e
Martin Pontius <m.pontius@52north.org>
2020-02-05 10:46:41 +0100

    Adapt preselect app for producers

--- app/js/SlupskTool/preselect.js
+++ app/js/SlupskTool/preselect.js
@@ -1,46 +1,86 @@
-define({
-    "run": function($page, routeInfo) {
+define(['wq/store'], function (ds) { 'use strict';
 
-        $page.on('click', 'a[data-wq-action=preselect]', function(evt) {
+var preselect = {
+    name: 'preselect'
+};
 
-          var $button = $(evt.target);
-          kindergarten = $button.parent().parent()[0].firstChild.innerText.trim();
-          localStorage.setItem("kindergarten", kindergarten); // it may be better to use wq's store.js app instead
+var $;
 
-        })
+preselect.init = function (config) {
+    $ = config && config.jQuery || window.jQuery;
+};
 
-        $page.ready( function() {
+preselect.run = function ($page, routeInfo) {
 
-          var kindergartenIDs = new Map( [[ "Przedszkole Miejskie nr 1 Zaczarowany Ogród", 1 ],
-          [ "Przedszkole Miejskie nr 2", 2 ],
-          [ "Przedszkole Miejskie nr 3", 3 ],
-          [ "Przedszkole Miejskie nr 4 Królestwo Skrzatów", 4 ],
-          [ "Przedszkole Miejskie nr 5", 5 ],
-          [ "Przedszkole Miejskie nr 6", 6 ],
-          [ "Przedszkole Miejskie nr 7", 7 ],
-          [ "Przedszkole Miejskie Integracyjne nr 8", 8 ],
-          [ "Przedszkole Miejskie nr 9 Akademia Uśmiechu", 9 ],
-          [ "Przedszkole Miejskie nr 10 Świat Fantazji", 10 ],
-          [ "Przedszkole Miejskie nr 11 Calineczka", 11 ],
-          [ "Przedszkole Miejskie nr 12 Niezapominajka", 12 ],
-          [ "Przedszkole Miejskie nr 15", 13 ],
-          [ "Przedszkole Miejskie nr 19 Słupskiego Chłopczyka", 14 ],
-          [ "Przedszkole Miejskie nr 23 Promyczek", 15 ],
-          [ "Przedszkole Miejskie nr 24 Słupski Niedźwiadek Szczęścia", 16 ],
-          [ "Przedszkole Miejskie nr 31 Bajkowa Kraina", 17 ],
-          [ "Przedszkole Miejskie Integracyjne nr 32 Tęczowa Kraina", 18 ]] );
+  // Save kindergarten from which link was clicked
+  $page.on('click', 'a[data-wq-action=preselect][class=kindergarten]', function(evt) {
 
-          if (localStorage.getItem("kindergarten") !== null) {
-            var kindergarten = localStorage.getItem("kindergarten").trim();
-            $('#kindergartendish-kindergarten_id').val(kindergartenIDs.get(kindergarten)).change();
-            $('#dishrating-kindergarten_id').val(kindergartenIDs.get(kindergarten)).change();
+    var $button = $(evt.target);
+    var kindergarten = $button.parent().parent()[0].firstChild.innerText.trim();
+    localStorage.setItem("kindergarten", kindergarten); // it may be better to use wq's store.js app instead
+
+  });
+
+  // Save producer from which link was clicked
+  $page.on('click', 'a[data-wq-action=preselect][class=producer]', function(evt) {
+
+    var $button = $(evt.target);
+    var producer = $button.parent().parent()[0].firstChild.innerText.trim();
+    localStorage.setItem("producer", producer);         // it may be better to use wq's store.js app instead
+
+  });
+
+  $page.ready( function() {
+
+    // Preselect kindergarten
+    if (localStorage.getItem("kindergarten") !== null) {
+
+      var kindergarten = localStorage.getItem("kindergarten").trim();
+
+      ds.get('/kindergartens').then(function(kindergartens) {
+
+        for (let k of kindergartens.list) {
+          if (k.name.trim() === kindergarten.trim()) {
+            $('#kindergartendish-kindergarten_id').val(k.id).change();
+            $('#dishrating-kindergarten_id').val(k.id).change();
             // alternative using direct js DOM manipulation--------------------
             // var $element = document.getElementById("kindergartendish-kindergarten_id-button");
             // $element.firstChild.innerText = kindergarten;
             // ----------------------------------------------------------------
+            break;
           }
+        }
 
-        })
+      });
 
-      }
+    }
+
+    // Preselect producer
+    if (localStorage.getItem("producer") !== null) {
+
+      var producer = localStorage.getItem("producer").trim();
+
+      ds.get('/producers').then(function(producers) {
+
+        for (let p of producers.list) {
+          if (p.name.trim() === producer.trim()) {
+            $('#producerinfo-producer_id').val(p.id).change();
+            break;
+          }
+        }
+
+      });
+
+    }
+
+
+  });
+
+
+
+};
+
+
+return preselect;
+
 });

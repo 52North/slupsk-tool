5480842f0f9ea0e79de9c2304f4b7eeff8d80629
Martin Pontius <m.pontius@52north.org>
2020-05-28 12:03:18 +0200

    Add setting files

---
+++ db/SlupskTool/settings/prod.py
@@ -0,0 +1,30 @@
+from .base import *
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SLUPSK_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# wq: Determine if we are running off django's testing server
+DEBUG_WITH_RUNSERVER = False
+
+ALLOWED_HOSTS = ["creatinginterfaces.demo.52north.org"]
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'slupsktool',
+        'USER': 'slupsktool',
+        'PASSWORD': os.environ.get('SLUPSK_DB_PW'),
+        'HOST': 'db-slupsk',
+        'PORT': '',
+    }
+}
+
+#SPATIALITE_LIBRARY_PATH = 'mod_spatialite.so'

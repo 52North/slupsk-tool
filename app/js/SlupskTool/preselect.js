a015a1eec42a054296c219053c951846cd26c2ae
Martin Pontius <m.pontius@52north.org>
2021-03-21 13:43:17 +0100

    Preselect kindergarten and date from latest selection

--- app/js/SlupskTool/preselect.js
+++ app/js/SlupskTool/preselect.js
@@ -30,6 +30,42 @@ preselect.run = function ($page, routeInfo) {
 
   });
 
+  // Save always the kindergarten that was selected latest (from "Add dishrating" form)
+  $page.on('change', 'select[id=dishrating-kindergarten_id]', function(evt) {
+
+    var $button = $(evt.target);
+    var kindergarten = $button.parent()[0].firstChild.innerText.trim();
+    ds.set('kindergarten', kindergarten);
+
+  });
+
+  // Save always the kindergarten that was selected latest (from "Add kindergarten dish" form)
+  $page.on('change', 'select[id=kindergartendish-kindergarten_id]', function(evt) {
+
+    var $button = $(evt.target);
+    var kindergarten = $button.parent()[0].firstChild.innerText.trim();
+    ds.set('kindergarten', kindergarten);
+
+  });
+
+  // Save always the date that was selected latest (from "Add dishrating" form)
+  $page.on('change', 'input[id=dishrating-date]', function(evt) {
+
+    var $button = $(evt.target);
+    var date = $button.val();
+    ds.set('date', date);
+
+  });
+
+  // Save always the date that was selected latest (from "Add kindergarten dish" form)
+  $page.on('change', 'input[id=kindergartendish-date]', function(evt) {
+
+    var $button = $(evt.target);
+    var date = $button.val();
+    ds.set('date', date);
+
+  });
+
   $page.ready( function() {
 
     // Preselect kindergarten
@@ -45,11 +81,20 @@ preselect.run = function ($page, routeInfo) {
               // var $element = document.getElementById("kindergartendish-kindergarten_id-button");
               // $element.firstChild.innerText = kindergarten;
               // ----------------------------------------------------------------
-              ds.set('kindergarten', "");
               break;
             }
           }
         });
+      }
+
+    });
+
+    // Preselect date
+    ds.get('date').then(function(date) {
+
+      if ( date !== null) {
+        $('#dishrating-date').val(date).change();
+        $('#kindergartendish-date').val(date).change();
       }
 
     });
@@ -62,7 +107,6 @@ preselect.run = function ($page, routeInfo) {
           for (let p of producers.list) {
             if (p.name.trim() === producer.trim()) {
               $('#producerinfo-producer_id').val(p.id).change();
-              ds.set('producer', "");
               break;
             }
           }

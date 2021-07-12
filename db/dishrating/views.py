e0ae79d966a3d2da93f41625575f7bb6f2a4e0e0
Martin Pontius <m.pontius@52north.org>
2021-07-12 17:28:00 +0200

    Add picture to dishrating (detail+list view)

---
+++ db/dishrating/views.py
@@ -0,0 +1,19 @@
+import os
+from wq.db.rest.views import ModelViewSet
+from .models import Dishrating
+from kindergartendish.models import Kindergartendish
+from django.conf import settings
+
+
+class DishratingViewSet(ModelViewSet):
+
+    def list(self, request, *args, **kwargs):
+        response = super(DishratingViewSet, self).list(request, *args, **kwargs)
+
+        # Add picture (url) of the corresponding dish to the list
+        for i in range(0, len(response.data['list'])):
+            dish_id = response.data['list'][i]['dish_id']
+            dish = Kindergartendish.objects.get(id=dish_id)
+            response.data['list'][i]['dish_picture'] = os.path.join(settings.BASE_DIR, dish.picture.url)
+
+        return response

8533e46393a3e06e08a5a09bdbc29d521c16593d
Martin Pontius <m.pontius@52north.org>
2021-07-15 12:01:35 +0200

    Fix error due to non-existent id

--- db/dishrating/views.py
+++ db/dishrating/views.py
@@ -13,7 +13,11 @@ class DishratingViewSet(ModelViewSet):
         # Add picture (url) of the corresponding dish to the list
         for i in range(0, len(response.data['list'])):
             dish_id = response.data['list'][i]['dish_id']
-            dish = Kindergartendish.objects.get(id=dish_id)
-            response.data['list'][i]['dish_picture'] = os.path.join(settings.BASE_DIR, dish.picture.url)
+            # Make sure the dishrating has a dish id
+            if dish_id is None:
+                continue
+            else:
+                dish = Kindergartendish.objects.get(id=dish_id)
+                response.data['list'][i]['dish_picture'] = os.path.join(settings.BASE_DIR, dish.picture.url)
 
         return response
